59cb5505ceb58a49757929aef7c602795897bc36
Makeli0 <markus.ahonenn@gmail.com>
2021-06-02 12:26:13 +0300

    spike trap added

---
+++ servants_of_the_ever_living/Assets/Scripts/Traps.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Traps : MonoBehaviour
+{
+
+    PlayerStats playerStats;
+
+    private void Start()
+    {
+        playerStats = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerStats>();
+
+         }
+
+    //When player collides with trap -> Instakill
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.transform.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<PlayerStats>().TakeDamage(playerStats.currentHealth);
+        }
+                
+    }
+
+}
