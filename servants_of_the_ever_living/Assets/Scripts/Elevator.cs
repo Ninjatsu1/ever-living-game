39ab3b1d0611ad414440e7d1714ac9fb55f0ff97
Nina Mäkinen <sherlocked1223@gmail.com>
2021-08-30 22:53:23 +0300

    Elevator added

---
+++ servants_of_the_ever_living/Assets/Scripts/Elevator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Elevator : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject ButtonCanvas;
+    private bool inArea = false;
+    private void Update()
+    {
+        if (inArea)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log("ere");
+                SceneManager.LoadScene(0);
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player")) {
+            ButtonCanvas.SetActive(true);
+            inArea = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            ButtonCanvas.SetActive(false);
+            inArea = false;
+        }
+    }
+}
